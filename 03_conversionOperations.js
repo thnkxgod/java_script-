59944be4643bfa0556ded931f71ae448755064fb
Rajendra Singh Arora <103161682+thnkxgod@users.noreply.github.com>
2023-11-26 17:09:42 +0530

    Add files via upload

---
+++ 03_conversionOperations.js
@@ -0,0 +1,19 @@
+
+// let score = "rajwesh";
+// console.log(typeof score);
+
+// let valueNumber = Number(score);
+// console.log(typeof(valueNumber));
+
+// console.log(valueNumber)
+
+
+// let isLoggedIn = 1;
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+let sum = 33;
+ let stringNum = String(sum)
+ console.log(stringNum)
+ console.log(typeof stringNum)
